b963a30103a2589e0b49f89646d8276d2f8d7976
Brian Donahue <brian.donahue@vitreosolutions.com>
2019-03-04 16:24:50 -0500

    Basic station request

--- serverless/toynbee.js
+++ serverless/toynbee.js
@@ -15,12 +15,40 @@ const LaunchRequestHandler = {
   }
 };
 
+const GetNextTrainsHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+    && handlerInput.requestEnvelope.request.intent.name === 'GetNextTrains';
+  },
+  handle(handlerInput) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+
+    const fromSlot = handlerInput.requestEnvelope.request.intent.slots.FromStation;
+    const toSlot = handlerInput.requestEnvelope.request.intent.slots.ToStation;
+    let from;
+    let to;
+    if (fromSlot && fromSlot.value) {
+      from = fromSlot.value.toLowerCase();
+    }
+    if (toSlot && toSlot.value) {
+      to = toSlot.value.toLowerCase();
+    }
+    const speechText = `You requested to go from ${from} to ${to}`
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .withSimpleCard('Hello World', speechText)
+      .getResponse();
+  }
+}
 const skillBuilder = Alexa.SkillBuilders.custom();
 
 module.exports = {
   handler: skillBuilder
   .addRequestHandlers(
-    LaunchRequestHandler 
+    LaunchRequestHandler,
+    GetNextTrainsHandler
   )
   .lambda()
 }
